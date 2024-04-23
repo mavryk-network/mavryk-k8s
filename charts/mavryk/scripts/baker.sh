e73e0d6c38c816e298286c99a780f6b0bc92a773
Tristan ALLAIRE <tristan.allaire@alarch.fr>
2024-04-23 14:41:02 +0200

    Tezos user fixed

--- charts/mavryk/scripts/baker.sh
+++ charts/mavryk/scripts/baker.sh
@@ -46,7 +46,7 @@ CLIENT="$TEZ_BIN/mavkit-client -d $CLIENT_DIR"
 CMD="$TEZ_BIN/mavkit-baker-$proto_command -d $CLIENT_DIR"
 
 # ensure we can run mavkit-client commands without specifying client dir
-ln -s /var/mavryk/client /home/mavryk/.mavryk-client
+ln -s /var/mavryk/client /home/tezos/.mavryk-client
 
 while ! $CLIENT rpc get chains/main/blocks/head; do
     sleep 5
